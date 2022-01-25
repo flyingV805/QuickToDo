75bd45c340629548d125782ec36f8b8c4f84e67b
V <v.petrov@indev.expert>
2022-01-25 16:05:35 +0600

    Main Screen concept

--- app/src/main/java/kz/flyingv/quicktodo/activity/main/MainActivity.kt
+++ app/src/main/java/kz/flyingv/quicktodo/activity/main/MainActivity.kt
@@ -2,13 +2,26 @@ package kz.flyingv.quicktodo.activity.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
 import kz.flyingv.quicktodo.R
+import kz.flyingv.quicktodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        val background = binding.bottomAppBar.background as MaterialShapeDrawable
+        val shapeAppearanceModel = background.shapeAppearanceModel
+        background.shapeAppearanceModel =
+            shapeAppearanceModel.toBuilder().setAllCorners(CornerFamily.ROUNDED, 22f).build()
     }
 
 }
