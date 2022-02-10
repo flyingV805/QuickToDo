5b5e420debbb545e3f99e381b72903ffd22b6d33
V <v.petrov@indev.expert>
2022-02-10 16:27:35 +0600

    Sync

---
+++ app/src/main/java/kz/flyingv/quicktodo/notifications/NotificationConstructor.kt
@@ -0,0 +1,11 @@
+package kz.flyingv.quicktodo.notifications
+
+class NotificationConstructor {
+
+    fun createCheckNotification(){
+
+
+
+    }
+
+}
--- app/src/main/java/kz/flyingv/quicktodo/notifications/NotificationsManager.kt
+++ app/src/main/java/kz/flyingv/quicktodo/notifications/NotificationsManager.kt
@@ -1,12 +1,19 @@
 package kz.flyingv.quicktodo.notifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
+import kz.flyingv.quicktodo.R
 import java.util.concurrent.TimeUnit
 
 class NotificationsManager {
+
+    private val notificationChannelID = ""
 
     fun setNotificationForTime(){
 
@@ -18,6 +25,20 @@ class NotificationsManager {
 
         requestBuilder.setInputData(data.build())
         WorkManager.getInstance(context).enqueue(requestBuilder.build())
+    }
+
+    private fun createNotificationChannels(context: Context){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = context.getString(R.string.empty)
+            val descriptionText = context.getString(R.string.empty)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(notificationChannelID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
 }
